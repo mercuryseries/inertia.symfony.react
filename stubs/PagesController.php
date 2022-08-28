8b69be0ebb5d86127103e7c8ee9a8e3d80520c19
Honore Hounwanou <mercuryseries@gmail.com>
2022-08-28 16:55:37 -0400

    WIP

--- stubs/PagesController.php
+++ stubs/PagesController.php
@@ -12,7 +12,7 @@ class PagesController extends AbstractController
     #[Route('/', name: 'app_home', options: ['expose' => true])]
     public function home(InertiaInterface $inertia): Response
     {
-        return $inertia->render('Home', ['name' => 'Honoré']);
+        return $inertia->render('Home', ['name' => 'John']);
     }
 
     #[Route('/about-us', name: 'app_about', options: ['expose' => true])]
