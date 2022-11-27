c314fc704fa328151c83331f5b53404ad1dd4254
Honore Hounwanou <mercuryseries@gmail.com>
2022-11-27 01:16:20 -0500

    WIP

---
+++ stubs/Layout2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <main role="main">{children}</main>
+    </>
+  );
+};
+
+export default (page) => <Layout>{page}</Layout>;
