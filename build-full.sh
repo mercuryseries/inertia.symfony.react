570b4ba2d0f62f118f7154e2aa8c76e275f41d8f
Honore Hounwanou <mercuryseries@gmail.com>
2023-01-24 02:24:31 -0500

    WIP

--- build-full.sh
+++ build-full.sh
@@ -33,7 +33,8 @@ function inertia.symfony.react {
     symfony new $PROJECT_NAME
     cd $PROJECT_NAME
     composer config --json extra.symfony.allow-contrib true
-    composer require twig-bundle encore serializer debug
+    composer require twig-bundle encore serializer
+    composer require debug --dev
     composer require rompetomp/inertia-bundle
     composer require friendsofsymfony/jsrouting-bundle
     composer require maker --dev
